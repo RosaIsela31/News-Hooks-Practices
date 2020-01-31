f8624e9cd219d8006d084df6b659566e5ac2323e
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-31 13:09:10 -0600

    Updates

---
+++ precache-manifest.b99989e9d8707721ad5df3aeadcd5823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8f941ee1c17d38363a0562adab7f28",
+    "url": "/News-Hooks-Practices/index.html"
+  },
+  {
+    "revision": "bcd456f16ed2bd1f9f65",
+    "url": "/News-Hooks-Practices/static/css/main.61688702.chunk.css"
+  },
+  {
+    "revision": "838b21888e6229839574",
+    "url": "/News-Hooks-Practices/static/js/2.c7dae4ab.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/News-Hooks-Practices/static/js/2.c7dae4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd456f16ed2bd1f9f65",
+    "url": "/News-Hooks-Practices/static/js/main.0751d7f8.chunk.js"
+  },
+  {
+    "revision": "0ac467136e0005d3afb2",
+    "url": "/News-Hooks-Practices/static/js/runtime-main.230f674c.js"
+  }
+]);
