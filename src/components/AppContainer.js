5ff7a213d353cc4912b358ca123f614c59bc31b4
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-31 12:54:58 -0600

    last commit for this project

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,26 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './Header';
 import Formulario from './Formulario'; 
+import ListadoNoticias from './ListadoNoticias';
 
 const AppContainer = () => {
 
   // Definir la categoria y noticias
   const [categoria, guardarCategoria] = useState('');
+  const [noticias, guadarNoticias] = useState([]);
+
+  // const APIKEY = '13da34c582b24cc19d5d50d7de01aa6c';
+  useEffect(() => {
+      const consultarAPI = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=13da34c582b24cc19d5d50d7de01aa6c`;
+
+        const respuesta = await fetch(url);
+        const noticias = await respuesta.json(); 
+
+        guadarNoticias(noticias.articles );
+      }
+      consultarAPI();
+  }, [categoria ]);
    
   return ( 
     <Fragment>
@@ -16,6 +31,9 @@ const AppContainer = () => {
         <Formulario 
           guardarCategoria={guardarCategoria} 
         /> 
+        <ListadoNoticias 
+            noticias={noticias}
+        />
       </div>
     </Fragment>
    );
